71e2d429a75bbe128a05ce151b2d37a5658148c8
DeveloperGitBash <55375940+DeveloperGitBash@users.noreply.github.com>
2021-08-26 20:45:48 +0530

    Update Main.java
    added new branch code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-System.out.println("sample to samole");
-        System.out.println("sample to samole1");
+       System.out.println("sample to samole");
+        System.out.println("welcome to new branch");
     }
 }
