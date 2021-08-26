ff897ceef1c136d4739a0a1379484213f7dd4b69
Mallikarjun <tirumala.mreddy@gmail.com>
2021-08-26 20:49:52 +0530

    updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
        System.out.println("sample to samole");
         System.out.println("welcome to new branch");
+        System.out.println("Please raise pull request");
     }
 }
